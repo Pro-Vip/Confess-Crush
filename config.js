df3ea4f0446d263368e4e9aab4452d5cabb350f6
Pro-Vip <93753690+Pro-Vip@users.noreply.github.com>
2021-11-05 11:18:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    titleWeb: "Anh yêu em",
+    introTitle: 'Trương Văn Hòa Nhân',
+    introDesc: `Trời hôm nay thật lạnh
+    Nhưng lại thiếu em để sưởi ấm :))`,
+    btnIntro: '<3 Love U <3',
+    title: 'yêu em từ cái nhìn đầu tiên 🥰',
+    desc: 'Vì nụ cười của em là liều thuốc bổ cho anh ',
     btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    btnNo: 'Yêu lắm <33',
+    question: 'Trên thế giới hơn 7 tỉ người nhưng người đẹp nhất vẫn là em <3',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Em là cả cuộc đời của anh <33333333',
+    mess: 'Yêu anh hông 🥰. Yêu thì bớt thức khuya lại nha 😘😘',
+    messDesc: 'Nghe lời anh thương.',
+    btnAccept: 'Dạ mãi yêuuu <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
